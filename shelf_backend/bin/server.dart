927690daa618a798fe51cc1ba896d45ec8eb39bb
Soares Ayoigbala <soaresayoigbala@gmail.com>
2025-08-19 21:39:51 +0100

    groq api fix

--- shelf_backend/bin/server.dart
+++ shelf_backend/bin/server.dart
@@ -1,7 +1,11 @@
+import 'package:shelf_backend/server/config/env.dart';
 import 'package:shelf_backend/server/server.dart';
 
 /// Main entry point for the server
 void main() async {
+  final env = EnvConfig(); // load .env here
+  print('Groq Key length loaded: ${env.groqApiKey.length}');
+  
   // Create and start the server
   final server = Server();
   await server.start();
